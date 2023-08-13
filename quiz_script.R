975464232a6685563a6e3cf0de2ad612f500cbf4
Katie Millar <ajh20fhu@uea.ac.uk>
2023-08-13 18:25:33 +0100

    Beginning the data list for the script.

---
+++ quiz_script.R
@@ -0,0 +1,7 @@
+quiz_data <- list(
+  list(
+    question = "Which one of these ISN'T a *drosophila* gene name?",
+    choices = c("Ken and Barbie", "Spock", "Darth Vader"),
+    answer = "Darth Vader",
+    image_filename = "images/"))
+
